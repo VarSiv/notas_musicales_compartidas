d8246cf8c6c89cbf4e9c620749302a727edc8f24
Stefsharon <victoriasamsung10@gmail.com>
2025-06-18 11:19:30 -0300

    juego

--- src/stores.js
+++ src/stores.js
@@ -1,9 +1,8 @@
+// src/stores.js
 import { writable } from 'svelte/store';
 
-// Este store guardará un objeto donde las claves serán 'Steffy', 'Rosita', 'Var'
-// y los valores serán el conteo de canciones reproducidas de cada persona.
 export const reproduccionesPorPersona = writable({
-    Steffy: 0,
-    Rosita: 0,
-    Var: 0
+    Steffy: { count: 0, lastPlayedSong: null }, // Ahora es un objeto con count y lastPlayedSong
+    Rosita: { count: 0, lastPlayedSong: null },
+    Var: { count: 0, lastPlayedSong: null }
 });
