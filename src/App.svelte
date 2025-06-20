00e271e89d7990738cc7f2d119ec4630ab7b6079
rositatz <rosita61tz@hotmail.com>
2025-06-20 01:31:50 -0300

    agrego scrolly

--- src/App.svelte
+++ src/App.svelte
@@ -129,7 +129,27 @@ Object.keys(cancionesPorDecada).forEach(decada => {
   }
   
   
-  
+  const slides = [
+    "En 2025, la canción más escuchada en Argentina fue “DTMF (Debí Tirar Más Fotos)” de Bad Bunny. Con un reguetón nostálgico pero bailable, el tema se volvió un clásico instantáneo: para perrear con el corazón roto y mover los recuerdos al ritmo del beat",
+    "Desde Estados Unidos, Beautiful Things de Benson Boone se convirtió en una de las canciones más escuchadas del 2025. Con su voz intensa y una letra que abraza el alma, la canción conectó con millones en todo el mundo. Una balada emotiva que habla de amor, pérdida y gratitud por las pequeñas cosas que hacen hermosa la vida.",
+    "Desde Francia, Est-ce que tu m’aimes? de GIMS volvió a sonar con fuerza en 2025. Un clásico moderno que mezcla melancolía y ritmo, preguntando con voz profunda lo que tantos temen decir: “¿Me amás?”. La canción cruzó fronteras con su estilo inconfundible, dejando eco en corazones de todo el mundo.",
+    "Desde Arabia Saudita, Die With a Smile de Lady Gaga y Bruno Mars conquistó el 2025 con una fusión inesperada de pop, soul y ritmos árabes. Un himno brillante que celebra la vida con estilo, actitud y una sonrisa final. Misterioso, magnético y poderoso… como el desierto al atardecer.",
+    "Desde Uganda llegó Baby (It Is a Crime), un hit que mezcló ritmos afrobeat con una historia de amor intenso y peligroso. Con beats vibrantes y una letra que duele, la canción se volvió himno en las pistas de África y más allá. Porque a veces… amar también puede ser un crimen.",
+    "Desde Japón, Mona Lisa de J-Hope se convirtió en una obra maestra del 2025. Un tema enigmático, con ritmos suaves y elegancia coreografiada, donde cada verso es una pincelada. Con su sonrisa críptica y su flow brillante, J-Hope hizo del silencio... puro arte pop.",
+    "Desde Nueva Zelanda, Ordinary de Alex Warren tocó fibras profundas en 2025. Una balada honesta sobre sentirse común en un mundo que exige brillar. Con guitarra suave y voz quebrada, convirtió lo simple en algo hermoso. Porque ser “ordinary” también es parte de lo extraordinario."
+    ]
+
+  function loadFlourishScrolly() {
+      const script = document.createElement('script')
+      script.src = "https://cdn.flourish.rocks/flourish-scrolly-v3.1.0.min.js"
+      script.type = "text/javascript"
+      script.onload = () => initFlourishScrolly()
+      document.body.appendChild(script)
+  }
+
+  onMount(() => {
+    loadFlourishScrolly()
+  });
 </script>
 
 <head>
@@ -299,7 +319,8 @@ Object.keys(cancionesPorDecada).forEach(decada => {
       En el centro de cada canción, <strong>una onda estática</strong> muestra el ritmo de la música, adoptando el color de su género musical. La amplitud de la onda refleja la danceability de la canción: cuanto más bailable, más pronunciada será la onda.
     </p>    
 
-  
+
+
 <div class="resultado-juego">
         <h2 class="titulo-juego">¿Con quién vibrás más?</h2>
         {#if totalReproducciones > 0}
@@ -351,6 +372,35 @@ Object.keys(cancionesPorDecada).forEach(decada => {
           <p>Explora un mapa interactivo con las canciones más reproducidas de 63 países en este momento.</p>
           <p>Hemos seleccionado 8 puntos destacados, de diferentes continentes, para llevarte en un viaje musical único.</p>
         </div>
+
+
+
+ 
+
+<!-- Contenedor de la story de flourish (scrolly)-->
+<div id="my-wrapper">
+  <!-- Reemplazar el ID de jeemplo por el de la story propia -->
+  <div class="flourish-embed" data-src="story/3175953" data-url="https://flo.uri.sh/story/3175953/embed" data-height="100vh">
+ <!-- <script src="https://public.flourish.studio/resources/embed.js"></script> -->
+</div> 
+
+
+
+  <!-- Iteramos sobre las distintas slides del componente de Flourish -->
+  {#each slides as slide, index}
+    <p>
+      {@html slide}
+      <!-- svelte-ignore a11y-missing-content -->
+      <a href={"#story/3175953/slide-" + (index + 1)}></a>
+    </p>
+  {/each}
+</div>
+
+<div class="texto_container">
+  <p class="texto_parrafo">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget mi eu nulla porta fringilla. Curabitur quis tincidunt purus. Duis nec neque et augue dictum blandit a eget neque. Phasellus tempor, metus et finibus pretium, eros est sagittis orci, vitae pellentesque nisl lectus et velit. Praesent fringilla mauris vitae magna sollicitudin, sit amet tincidunt dolor facilisis. Fusce quis augue et tortor interdum bibendum. Nam dolor elit, posuere nec tortor non, molestie congue justo. Praesent nisl arcu, consectetur sed hendrerit eget, interdum ac nisi. Vestibulum tempor mattis purus. Aliquam tempus neque sed velit convallis tempor. Cras sodales nunc lorem. Nulla pharetra malesuada consequat. Nulla elementum odio non mauris mollis suscipit. Praesent consectetur nunc eget ex varius blandit. Sed id facilisis tellus.
+  </p>
+</div>
 
     <Footer/>
 </body>
